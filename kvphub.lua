74e7e6ff836b5b9a22f618e1005d18c2d932ec7f
10memati <80392076+10memati@users.noreply.github.com>
2023-10-14 19:51:45 +0000

    Update kvphub.lua

--- kvphub.lua
+++ kvphub.lua
@@ -1,7 +1,9 @@
-local lt2 = "13822889"
-local lb = "662417684"
+local lt2 = "13822889" -- Lumber Tycoon 2
+local lb = "662417684" -- Lucky Blocks
+local psx = "6284583030" -- Pet Simulator X
 
-if tonumber(game.placeId) == tonumber(lb) then
+-- Lucky Blocks
+if tostring(game.placeId) == tostring(lb) then
 local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/10memati/Orion/main/source')))()
 local Window = OrionLib:MakeWindow({Name = "KVP Hub | ❓ Lucky Blocks Battleground", HidePremium = false, SaveConfig = true, ConfigFolder = "KVP Hub"})
 
@@ -162,9 +164,97 @@ Tab:AddButton({
 })
 
 --
-OrionLib:Init() 
-	
-else 
+OrionLib:Init()
+elseif tostring(game.placeId) == tostring(psx) then
+   local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/10memati/Orion/main/source')))()
+local Window = OrionLib:MakeWindow({Name = "KVP Hub | Pet Simulator X! 🐾", HidePremium = false, SaveConfig = true, ConfigFolder = "KVP Hub"})
+
+local Tab = Window:MakeTab({
+	Name = "Player",
+	Icon = "rbxassetid://4483345998",
+	PremiumOnly = false
+})
+
+local Section = Tab:AddSection({
+ Name = "Player"
+})
+
+local GameTab = Window:MakeTab({
+    Name = "Game",
+    Icon = "rbxassetid://4483345998",
+    PremiumOnly = false
+})
+
+local GameSection = GameTab:AddSection({
+    Name = "Game"
+})
+
+  -- Player Sets
+  -- Speed
+Tab:AddTextbox({
+    Name = "Speed",
+    Default = "20",
+    TextDisappear = false,
+    Callback = function(Value)
+        local s = Value:gsub("[%a]", "")
+        local hiz = tonumber(s)
+        
+        if hiz > 300 then
+            hiz = 300
+            end
+           Value = hiz
+        
+        game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = tonumber(hiz)
+end
+})
+
+  -- Jump Power
+Tab:AddTextbox({
+    Name = "Jump Power",
+    Default = "50",
+    Callback = function(Value)
+        local s = Value:gsub("[%a]", "")
+        local jump = tonumber(s)
+        
+        if jump > 500 then
+            jump = 500
+            end
+
+	Value = jump
+        
+        game.Players.LocalPlayer.Character.Humanoid.JumpPower = tonumber(jump)
+    end
+})
+
+  -- Infinite Jump
+local InfiniteJumpEnabled = false
+Tab:AddToggle({
+    Name = "Infinite Jump",
+    Default = false,
+    Callback = function(Value)
+        InfiniteJumpEnabled = Value
+    end
+})
+
+game:GetService("UserInputService").JumpRequest:Connect(function()
+    if InfiniteJumpEnabled then
+        game:GetService("Players").LocalPlayer.Character:FindFirstChildOfClass("Humanoid"):ChangeState("Jumping")
+    end
+end)
+
+--[[
+	--All Server Players
+local players = game:GetService("Players"):GetPlayers()
+local playerNames = {}
+
+for _, player in pairs(players) do
+    table.insert(playerNames, player.Name)
+end
+--]]
+
+--
+OrionLib:Init()
+else
 -- Error
 game:GetService("StarterGui"):SetCore("SendNotification",{
 	Title = 'Error', -- Required

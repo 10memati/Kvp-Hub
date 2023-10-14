227323cca7933a306a1ae77eb525346d6279491a
mematibassss65 <80392076+10memati@users.noreply.github.com>
2023-10-14 13:00:38 +0300

    Update kvphub.lua

--- kvphub.lua
+++ kvphub.lua
@@ -1,38 +1,4 @@
 local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/10memati/Orion/main/source')))()
-
-local Player = game.Players.LocalPlayer
-local UIS = game:GetService("UserInputService")
-Player.CharacterAdded:Wait()
-local hrp = Player.Character:WaitForChild("HumanoidRootPart")
-local RunService = game:GetService("RunService")
-local KeyFlags = {
-    P = false
-}
-
--- Bu bayrak "fly" durumunu belirtir
-local isFlying = false
-
-function move(delta)
-    Player.Character:WaitForChild("HumanoidRootPart").CFrame *= CFrame.new(Vector3.new(0, 10 * delta, 0))
-end
-
-UIS.InputBegan:Connect(function(input, gameProcessedEvent)
-    if gameProcessedEvent then return end
-
-    if input.UserInputType == Enum.UserInputType.Keyboard then
-        if input.KeyCode == Enum.KeyCode.P then
-            move(1)
-            KeyFlags.P = true
-        end
-    end
-end)
-
-UIS.InputEnded:Connect(function(input, gameProcessedEvent)
-    if input.KeyCode == Enum.KeyCode.P then
-        KeyFlags.P = false
-    end
-end)
-
 local Window = OrionLib:MakeWindow({Name = "KVP Hub", HidePremium = false, SaveConfig = true, ConfigFolder = "KVP Hub"})
 
 local Tab = Window:MakeTab({
@@ -78,21 +44,5 @@ Tab:AddTextbox({
         game.Players.LocalPlayer.Character.Humanoid.JumpPower = tonumber(jump)
     end
 })
-
-Tab:AddToggle({
-    Name = "Fly Toggle",
-    Default = false, -- Başlangıçta "false" olarak ayarlayın
-    Callback = function(Value)
-        isFlying = Value
-    end
-})
-
-RunService.RenderStepped:Connect(function(delta)
-    if isFlying and KeyFlags.P then
-        -- Eğer "fly" açıksa ve "P" tuşuna basılıysa, hareketi gerçekleştir
-        move(1)
-    end
-    -- Diğer kodlarınızı buraya ekleyin
-end)
 
 OrionLib:Init()

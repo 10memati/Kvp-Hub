dde503caea45aa89251ad7933337821297a0afe5
mematibassss65 <80392076+10memati@users.noreply.github.com>
2023-11-13 18:34:58 +0300

    Update kvphub.lua

--- kvphub.lua
+++ kvphub.lua
@@ -3,6 +3,8 @@ local lb = "662417684" -- Lucky Blocks
 local los = "3101667897" -- Legend of Speed
 local lt2 = "13822889" -- Lumber Tycoon 2
 
+print("Game Place ID:", game.PlaceId)
+
 if tonumber(game.PlaceId) == tonumber(nl) then
-	loadstring(game:HttpGet("https://raw.githubusercontent.com/10memati/Kvp-Hub/main/games/lucky-blocks.lua"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/10memati/Kvp-Hub/main/games/lucky-blocks.lua"))()
 end

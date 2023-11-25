70a35c2812a2d168b3a88b1f8f5653e4350e571e
mematibassss65 <80392076+10memati@users.noreply.github.com>
2023-11-26 02:26:03 +0300

    Update kvphub.lua

--- kvphub.lua
+++ kvphub.lua
@@ -2,8 +2,32 @@ local lb = "662417684" -- Lucky Blocks
 local los = "3101667897" -- Legend of Speed
 local lt2 = "13822889" -- Lumber Tycoon 2
 
+function hata(text)
+    local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/10memati/Orion/main/source')))()
+OrionLib:MakeNotification({
+        Name = "Error",
+        Content = text,
+        Image = "rbxassetid://4483345998",
+        Time = 5
+    })
+end
+
+function ok(text)
+    local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/10memati/Orion/main/source')))()
+OrionLib:MakeNotification({
+        Name = "Success",
+        Content = text,
+        Image = "rbxassetid://4483345998",
+        Time = 5
+    })
+end
+
 if tostring(game.PlaceId) == tostring(lb) then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/10memati/Kvp-Hub/main/games/lucky-blocks.lua"))()
+    ok('Successfully loaded for Lucky Blocks!')
 elseif tostring(game.PlaceId) == tostring(los) then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/10memati/Kvp-Hub/main/games/legendsofspeed.lua"))()
+    ok('Successfully loaded for Legends of Speed!')
+else
+    hata('This game is not supported!')
 end

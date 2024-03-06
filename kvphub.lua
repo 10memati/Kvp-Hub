2b7f436648572f9168aa56427bf3ba9edf858b72
mematibassss65 <80392076+10memati@users.noreply.github.com>
2024-03-06 23:36:03 +0300

    Update kvphub.lua

--- kvphub.lua
+++ kvphub.lua
@@ -1,7 +1,7 @@
 local lb = "662417684" -- Lucky Blocks
 local los = "3101667897" -- Legend of Speed
 local lt2 = "13822889" -- Lumber Tycoon 2
-
+local aws = "13127800756" -- Arm Wrestle Simulator
 function hata(text)
     local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/10memati/Orion/main/source')))()
 OrionLib:MakeNotification({
@@ -28,6 +28,9 @@ if tostring(game.PlaceId) == tostring(lb) then
 elseif tostring(game.PlaceId) == tostring(los) then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/10memati/Kvp-Hub/main/games/legendsofspeed.lua"))()
     ok('Successfully loaded for Legends of Speed!')
+elseif tostring(game.placeId) == tostring(los) then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/10memati/Kvp-Hub/main/games/armwrestlesimulator.lua"))()
+    ok('Successfully loaded for Arm Wrestle Simulator!')
 else
     hata('This game is not supported by KVP Hub!')
 end
